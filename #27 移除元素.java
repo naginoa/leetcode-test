f8731514ad9bfd30475415b497917f7906fc93ed
任欣源 <renxinyuan3@163.com>
2020-12-22 01:54:36 +0800

    Update #27 移除元素.java
    优化

--- #27 移除元素.java
+++ #27 移除元素.java
@@ -3,18 +3,14 @@ class Solution {
         int flag=0;
         int len=nums.length - 1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==val){
-                nums[i]=Integer.MIN_VALUE;
-            }
-            else flag++;
+            if(nums[i]!=val)flag++;
         }
         for(int i=0;i<flag;i++){
-            if(nums[i]==Integer.MIN_VALUE){
-                while(nums[len]==Integer.MIN_VALUE)len--;
+            if(nums[i]==val){
+                while(nums[len]==val)len--;
                 int t=nums[i];
                 nums[i] = nums[len];
                 nums[len] = t;
-                //System.out.println(1111);
             }
         }
         return flag;
